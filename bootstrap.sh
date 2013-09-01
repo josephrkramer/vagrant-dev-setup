4b24e8bb64ccba0991f3a67ae4cd2512ab81cb56
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2013-08-31 20:12:10 -0400

    Upgrade emacs to 24
    Since Ubuntu doesn't have an emacs24 package, this builds emacs24
    from the source.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 apt-get update
-apt-get install -y emacs23
+#apt-get install -y emacs23
+
+#emacs24
+apt-get install -y make libjpeg-dev libpng-dev libgif-dev libtiff-dev libncurses-dev
+wget -c http://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.gz
+tar xfz emacs-24.3.tar.gz
+cd emacs-24.3
+./configure
+make
+make install
+
 apt-get install -y git
 apt-get install -y openjdk-7-jre-headless
