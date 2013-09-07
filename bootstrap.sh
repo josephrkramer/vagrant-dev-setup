c3fb0f7b9ddb5ce6c2b7683b5d07b830cc41ea3d
Joseph Kramer <joseph.ryan.kramer@gmail.com>
2013-09-07 16:24:34 -0400

    Only install/compile missing software

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,29 +3,37 @@
 apt-get update
 
 #emacs24
-apt-get install -y make libjpeg-dev libpng-dev libgif-dev libtiff-dev libncurses-dev
-wget -c http://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.gz
-tar xfz emacs-24.3.tar.gz
-cd emacs-24.3
-./configure
-make
-make install
-cd ..
+if [ ! `which emacs` ] ; then
+    apt-get install -y make libjpeg-dev libpng-dev libgif-dev libtiff-dev libncurses-dev
+    wget -c http://ftp.gnu.org/gnu/emacs/emacs-24.3.tar.gz
+    tar xfz emacs-24.3.tar.gz
+    cd emacs-24.3
+    ./configure
+    make
+    make install
+    cd ..
+fi
 
 #install and configure git
-apt-get install -y git
-su - vagrant -c "git config --global user.name \"Joseph Kramer\""
-su - vagrant -c "git config --global user.email joseph.ryan.kramer@gmail.com"
-su - vagrant -c "git config --global core.editor emacs"
-su - vagrant -c "git config --global color.ui auto"
+if [ ! `which git` ] ; then
+    apt-get install -y git
+    su - vagrant -c "git config --global user.name \"Joseph Kramer\""
+    su - vagrant -c "git config --global user.email joseph.ryan.kramer@gmail.com"
+    su - vagrant -c "git config --global core.editor emacs"
+    su - vagrant -c "git config --global color.ui auto"
+fi
 
 #Java7 JDK
-apt-get install -y openjdk-7-jdk
+if [ ! `which java` ] ; then
+    apt-get install -y openjdk-7-jdk
+fi
 
 #sbt for scala
-apt-get install -y curl
-wget -c http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.0/sbt.deb
-dpkg -i sbt.deb
+if [ ! `which sbt` ] ; then
+    apt-get install -y curl
+    wget -c http://repo.scala-sbt.org/scalasbt/sbt-native-packages/org/scala-sbt/sbt/0.13.0/sbt.deb
+    dpkg -i sbt.deb
+fi
 
 #download emacs config
 su - vagrant -c "wget -c https://raw.github.com/josephrkramer/emacs-config/master/.emacs"
